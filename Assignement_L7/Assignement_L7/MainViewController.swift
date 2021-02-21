87441c3d8328b8a2e8cc2e154c39f1a3aaf27881
Titanscull <ispetrovskyi@gmail.com>
2021-02-21 11:01:27 +0200

    MainViewController set up

--- Assignement_L7/Assignement_L7/MainViewController.swift
+++ Assignement_L7/Assignement_L7/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class MainViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var nameTextField: UITextField!
     
