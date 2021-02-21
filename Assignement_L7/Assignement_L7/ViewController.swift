dc492f2dd5482d4bc327b2ae86e54656b009a965
Titanscull <ispetrovskyi@gmail.com>
2021-02-21 10:59:10 +0200

    Blank project Lables & text field added

---
+++ Assignement_L7/Assignement_L7/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Assignement_L7
+//
+//  Created by Ievgen Petrovskiy on 21.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nameSurnameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.nameTextField.delegate = self
+        
+        nameSurnameLabel.isHidden = true
+        
+    }
+    
+    /// Use return nutton to hide keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return nameTextField.resignFirstResponder()
+    }
+    
+    /// Use tap on screen to hide keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+}
+
